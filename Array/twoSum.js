946897dc17f1234cb9bc72be88c0c8d3f1355ff4
KhoaDang <dangkhoa1410@gmail.com>
2021-05-28 00:03:14 -0400

    Update Leetcode link

---
+++ Array/twoSum.js
@@ -0,0 +1,20 @@
+// Leetcode https://leetcode.com/problems/two-sum/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    for (let i=0; i<nums.length; i++){
+        for (let j=1; j<=nums.length-1; j++){
+            if(i === j){
+                continue
+            }
+            if(nums[i]+nums[j] === target){
+            return [i,j]
+            }
+        }
+        
+    }
+};
